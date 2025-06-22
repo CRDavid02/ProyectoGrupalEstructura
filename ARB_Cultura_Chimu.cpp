978b80b87d0abea2b7eff24898d9b21bc6877f2b
CRDavid02 <73392625@continental.edu.pe>
2025-06-21 22:49:19 -0500

    Recorrido In_Orden

--- ARB_Cultura_Chimu.cpp
+++ ARB_Cultura_Chimu.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+
+
+
+// --------------------------------------------------
+// INTEGRANTE: Halber David Ccapchi Rios– Funciones de Recorrido
+// --------------------------------------------------
+
+// Muestra el árbol en recorrido inorden (izq - raíz - der)
+void inorden(Nodo* raiz) {
+    if (raiz == NULL) return;
+    inorden(raiz->izquierda);
+    cout << raiz->id << " - " << raiz->nombre << endl;
+    inorden(raiz->derecha);
+}
