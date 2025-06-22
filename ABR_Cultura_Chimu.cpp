904df6b18a8bfb43cb99c394c62d6cd3a739378f
johan20254 <76246168@continental.edu.pe>
2025-06-21 22:53:36 -0500

    Update ABR_Cultura_Chimu.cpp

--- ABR_Cultura_Chimu.cpp
+++ ABR_Cultura_Chimu.cpp
@@ -15,3 +15,32 @@ void inorden(Nodo* raiz) {
 }
 
 //funcion de eliminacion
+Nodo* encontrarMin(Nodo* raiz) {
+    while (raiz->izquierda != NULL)
+        raiz = raiz->izquierda;
+    return raiz;
+}
+Nodo* eliminar(Nodo* raiz, int id) {
+    if (!raiz) return NULL;
+
+    if (id < raiz->id)
+        raiz->izquierda = eliminar(raiz->izquierda, id);
+    else if (id > raiz->id)
+        raiz->derecha = eliminar(raiz->derecha, id);
+    else {
+if (!raiz->izquierda) {
+            Nodo* temp = raiz->derecha;
+            delete raiz;
+            return temp;
+        } else if (!raiz->derecha) {
+            Nodo* temp = raiz->izquierda;
+            delete raiz;
+            return temp;
+        }
+ Nodo* temp = encontrarMin(raiz->derecha);
+        raiz->id = temp->id;
+        raiz->nombre = temp->nombre;
+        raiz->derecha = eliminar(raiz->derecha, temp->id);
+    }
+    return raiz;
+}
